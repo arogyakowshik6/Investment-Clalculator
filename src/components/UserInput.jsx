fb22e027cd60b0a13884f184f2027f0a9706e5e0
Kuppala Arogya Kowshik <arogyakowshik6@gmail.com>
2026-03-12 11:53:41 +0000

    Binding the inputvalues

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React , {useState}from 'react'
+
 import '../App.css'
 
-const UserInput = () => {
+
+export const UserInput = () => {
+  const [inputCust, setInputCust] = useState ({
+    BegInvestments: 40000,
+    AnnInvestments : 12000,
+    returnInv : 7,
+    yearInv :38
+
+  })
+  function callUserInput(inputIde, val){
+    setInputCust((prev)=>({
+      ...prev,
+      [inputIde] : val
+    }))
+  }
   return(
     <section id = "user-input">
       <div className = "input-group" >
         <p>
           <label>Beginning Investments</label>
-          <input Type = "number"></input>
+
+          <input Type = "number" value ={inputCust.BegInvestments}required onChange = {()=> callUserInput('BegInvestemnt'.e.target.value)}/>
         </p>
         <p>
           <label>Annual Investment </label>
-          <input Type = "number"></input>
+          <input Type = "number" value = {inputCust.AnnInvestments}required onChnage = {()=> callUserInput ('AnnInvestments'.e.target.value)}/>
         </p>
         <p>
           <label>Return expected</label>
-          <input Type = "number"></input>
+          <input Type = "number" value = {inputCust.returnInv}required onChange = {()=> callUserInput ('returnInv'.e.target.value)}/>
         </p>
         <p>
           <label>Yearly Investment</label>
-          <input Type = "number"></input>
+          <input Type = "number"value = {inputCust.yearInv} required onChange = {()=> callUserInput ('yearInv'.e.target.value)}/>
         </p>
       </div>
         
