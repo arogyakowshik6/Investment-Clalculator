1a5320bf053ae732dc976bdaf18c268a578401fb
Kuppala Arogya Kowshik <arogyakowshik6@gmail.com>
2026-03-12 11:29:26 +0000

    Created an input feild component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx'
 import './App.css'
+import UserInput from './components/UserInput.jsx'
 
 function App() {
   
   return (
     <>
       <Header />
+      <UserInput />
       {/* additional components goes here */}
     </>
   )
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,7 +3,25 @@ import '../App.css'
 
 const UserInput = () => {
   return(
-    <section>
+    <section id = "user-input">
+      <div className = "input-group" >
+        <p>
+          <label>Beginning Investments</label>
+          <input Type = "number"></input>
+        </p>
+        <p>
+          <label>Annual Investment </label>
+          <input Type = "number"></input>
+        </p>
+        <p>
+          <label>Return expected</label>
+          <input Type = "number"></input>
+        </p>
+        <p>
+          <label>Yearly Investment</label>
+          <input Type = "number"></input>
+        </p>
+      </div>
         
     </section>
   ) 
